c0b2315d67200776346977316511d0973f184108
BlaqArch <shayanariz99@gmail.com>
2021-04-21 12:10:09 +0530

    encryptedMessage

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EncryptedMsg
+
+
+This is a encrypter and decrypter for your messages or you can say important text you don't want others to see.
+Also this is my first GitHub 'Completed' project.
+
+How to use:
+1) First Create a password.
+2) Choose wether you want to encrypt or decrypt. (Ofcourse you should choose encrypt if none of your file is encrypted or it will give an error.)
+3) If you want to see the encrypted message, just re-run the script and then select decrypt.
+4) Enter your password, and you can see your encrypted message.
---
+++ encrypter.py
@@ -0,0 +1,65 @@
+from os import path
+
+alphabets = 'abcdefghijklmnopqrstuvwxyz'
+key = 5
+def enter_Message():
+    message = input('Message: ')
+    encrypt_Message = ''
+    for i in message:
+        pos = alphabets.find(i)
+        newpos = (pos+5)%26
+        encrypt_Message += alphabets[newpos]
+    msg_File = open('message.txt', 'w')
+    msg_File.write(encrypt_Message)
+    msg_File.close()
+
+def decrypt_Message():
+    decrypt_msg = ''
+    msg_File = open('message.txt', 'r')
+    msg_file_decrypt_read = msg_File.read()
+    msg_File.close()
+    for i in msg_file_decrypt_read:
+        depos = alphabets.find(i)
+        newdepos = (depos-5)%26
+        decrypt_msg += alphabets[newdepos]
+    password_File = open('password.txt', 'r')
+    decrypt_encrypted_password = password_File.read()
+    password_File.close()
+    decrypt_Password = ''
+    for i in decrypt_encrypted_password:
+        deposition = alphabets.find(i)
+        newdeposition = (deposition-5)%26
+        decrypt_Password += alphabets[newdeposition]
+    enter_Pwd = input('Password: ')
+    if enter_Pwd == decrypt_Password:
+        print(decrypt_msg)
+
+
+def take_Password():
+    password = input("Password: ")
+
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    key = 5
+    encrypt_Password = ''
+    for i in password:
+        position = alphabets.find(i)
+        newposition = (position+5)%26
+        encrypt_Password += alphabets[newposition]
+    password_File = open('password.txt', 'w')
+    password_File.write(encrypt_Password)
+    password_File.close()
+    ask_Option()
+
+def ask_Option():
+    option = input('DO you wnt to encrypt or decrypt? ')
+    if option == 'encrypt':
+        enter_Message()
+    elif option == 'decrypt':
+        decrypt_Message()
+
+if path.exists('password.txt') == True:
+    ask_Option()
+
+
+else:
+    take_Password()
